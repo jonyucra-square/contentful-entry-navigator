ff7c91483157a0670a1e0276cefe9fece238d264
Jonatan Yucra Rodriguez <jonatany@squareup.com>
2021-12-14 14:02:35 -0500

    Add button to redirect to local version of supportcenter article from support article contentful entry.

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 let contentfulToken = document.getElementById("contentful-token");
-let externalId = document.getElementById('external-id');
+let articleUrl = document.getElementById('article-url');
+let navigate = document.getElementById('navigate');
 
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   let currentUrl = tabs[0].url;
@@ -9,6 +10,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   fetch(`http://localhost:3001/externalId/${contentfulToken.innerHTML}`).then((res) => {
     return res.json()
   }).then((res => {
-    externalId.innerHTML = res.externalId;
+    articleUrl.innerHTML = `http://localhost:3000/help/article/${res.externalId}`;
+    navigate.href = articleUrl.innerHTML;
   }));
 });
