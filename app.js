eb197351b4ab205fc1fffa9415f370ec04bc78ee
Jonatan Yucra Rodriguez <jonatany@squareup.com>
2021-12-14 13:24:09 -0500

    Make a call from the client to the server.

--- app.js
+++ app.js
@@ -22,10 +22,16 @@ app.listen('3001', () => {
 
 app.get('/externalId', (req, res) => {
 
+  console.log("Receiving Request...");
   let sql = 'SELECT * FROM articles WHERE contentful_token="7vDv2ArVDONdFlfaFc3wh3";';
   db.query(sql, (err, result) => {
     let externalId = `${result[0].external_id}`;
-    res.send(externalId);
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    const body = {
+      "externalId" : externalId
+    }
+    res.send(body);
   });
 
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,14 @@
 let token = document.getElementById("contentful-token");
-token.innerHTML = window.location.href;
+let externalId = document.getElementById('external-id');
 
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   let currentUrl = tabs[0].url;
   const splitUrl = currentUrl.split("/");
   token.innerHTML = splitUrl[splitUrl.length - 1];
+
+  fetch('http://localhost:3001/externalId').then((res) => {
+    return res.json()
+  }).then((res => {
+    externalId.innerHTML = res.externalId;
+  }));
 });
