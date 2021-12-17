a08eec1aac565b13333cb9ff86576fc5760a24ce
Jonatan Yucra Rodriguez <jonatany@squareup.com>
2021-12-17 02:37:23 -0500

    Handle going from article to contentful entry.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.listen('3001', () => {
 
 app.get('/externalId/:contentful_token', (req, res) => {
 
-  console.log("Receiving Request...");
+  console.log("Receiving Request For External Id...");
   let sql = `SELECT * FROM articles WHERE contentful_token="${req.params.contentful_token}";`;
   db.query(sql, (err, result) => {
     let externalId = `${result[0].external_id}`;
@@ -30,6 +30,22 @@ app.get('/externalId/:contentful_token', (req, res) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     const body = {
       "externalId" : externalId
+    }
+    res.send(body);
+  });
+
+});
+
+app.get('/contentfulToken/:external_id', (req, res) => {
+
+  console.log("Receiving Request For Token...");
+  let sql = `SELECT * FROM articles WHERE external_id="${req.params.external_id}";`;
+  db.query(sql, (err, result) => {
+    let contentfulToken = `${result[0].contentful_token}`;
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    const body = {
+      "contentfulToken" : contentfulToken
     }
     res.send(body);
   });
--- popup.js
+++ popup.js
@@ -1,16 +1,34 @@
-let contentfulToken = document.getElementById("contentful-token");
-let articleUrl = document.getElementById('article-url');
+let articleContentfulToken = document.getElementById("contentful-token");
+let articleExternalId = document.getElementById('article-external-id');
 let navigate = document.getElementById('navigate');
 
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  let currentUrl = tabs[0].url;
-  const splitUrl = currentUrl.split("/");
-  contentfulToken.innerHTML = splitUrl[splitUrl.length - 1];
 
-  fetch(`http://localhost:3001/externalId/${contentfulToken.innerHTML}`).then((res) => {
-    return res.json()
-  }).then((res => {
-    articleUrl.innerHTML = `http://localhost:3000/help/article/${res.externalId}`;
-    navigate.href = articleUrl.innerHTML;
-  }));
+  let currentUrl = new URL(tabs[0].url);
+
+  if (currentUrl.hostname == 'app.contentful.com') {
+    const splitUrl = currentUrl.toString().split("/");
+    const contentfulToken = splitUrl[splitUrl.length - 1];
+    articleContentfulToken.innerHTML = contentfulToken;
+
+    fetch(`http://localhost:3001/externalId/${contentfulToken}`).then((res) => {
+      return res.json()
+    }).then((res => {
+      articleExternalId.innerHTML = res.externalId;
+      navigate.innerHTML = 'Go To Article';
+      navigate.href = `http://localhost:3000/help/us/en/article/${res.externalId}`;
+    }));
+  } else { // TODO: Check that this url is actually a version of supportcenter
+    const splitUrl = currentUrl.toString().split("/");
+    const externalId = splitUrl[splitUrl.length - 1];
+    articleExternalId.innerHTML = externalId;
+    fetch(`http://localhost:3001/contentfulToken/${externalId}`).then((res) => {
+      return res.json()
+    }).then((res => {
+      articleContentfulToken.innerHTML = res.contentfulToken;
+      navigate.innerHTML = 'Go To Contentful Entry';
+      navigate.href = `https://app.contentful.com/spaces/gc4s9mi2asix/environments/development/entries/${res.contentfulToken}`;
+    }));
+  }
+
 });
